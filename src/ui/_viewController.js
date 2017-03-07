afe2d4deb650a55b5427c86a86b3640665f37c1c
Banks, Martin <anotherbanksy@gmail.com>
2017-03-07 19:06:08 +1100

    add color view

--- src/ui/_viewController.js
+++ src/ui/_viewController.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import AtomsText from './views/AtomsText'
+import AtomsColor from './views/AtomsColor'
 
 export const viewController = {
 	atoms: {
 		text: {
 			template: <AtomsText />
+		},
+		color: {
+			template: <AtomsColor />
 		}
 	}
 }
