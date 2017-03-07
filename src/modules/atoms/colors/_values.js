67af0bac9b2871535bd582197a833cb670f2b91b
Banks, Martin <anotherbanksy@gmail.com>
2017-03-07 19:06:41 +1100

    add color values

---
+++ src/modules/atoms/colors/_values.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const colors = {
+	neutral: {
+		light: "#e2e2e2",
+		midLight: "#cccccc",
+		mid: "#888888",
+		middark: "#666666",
+		dark: "#333333",
+	},
+	red: {
+		regular: '#ff0000'
+	},
+	green: {
+		regular: '#00ff00'
+	},
+	blue: {
+		regular: '#0000ff'
+	}
+}
+
