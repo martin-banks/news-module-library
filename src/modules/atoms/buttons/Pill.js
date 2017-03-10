b64a64a7e35da956022bdf2595071a6395acd44d
Banks, Martin <anotherbanksy@gmail.com>
2017-03-10 19:35:25 +1100

    new pill template and styles

---
+++ src/modules/atoms/buttons/Pill.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import style from './buttonStyles.css'
+import MaterialIcon from '../icon/MaterialIcon.js'
+
+export default class ButtonPill extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = { 
+			icon: 'play_arrow',
+			text: 'Show more',
+			iconClass: ''
+		}	
+	}
+
+	render(){
+		return (
+			<div className={style.Pill} >
+				<span>
+					{this.props.text || this.state.text}
+				</span>
+				<div className={style.pillIconContainer} >
+					<MaterialIcon icon={this.state.icon}/>
+				</div>
+
+			</div>
+		)
+	}
+
+}
+
+
+ButtonPill.propTypes = {
+	text: React.PropTypes.string
+}
