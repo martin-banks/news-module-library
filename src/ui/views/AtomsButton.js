97469c3932f62d3870de89a1dbe4562bc0941a4f
Banks, Martin <anotherbanksy@gmail.com>
2017-03-10 12:05:38 +1100

    additional button atom templates

--- src/ui/views/AtomsButton.js
+++ src/ui/views/AtomsButton.js
@@ -2,11 +2,16 @@ import React from 'react'
 import {sampleText} from '../../sampleContent/text'
 
 import ButtonStart from '../../modules/atoms/buttons/Start'
+import ButtonRoundIcon from '../../modules/atoms/buttons/RoundIcon'
+import ButtonSquareText from '../../modules/atoms/buttons/SquareText'
+
 import PreviewContainer from '../viewModules/PreviewContainer'
 import style from '../../App.css'
 
 const components = {
 	ButtonStart: <ButtonStart text='My test button'/>,
+	roundButton: <ButtonRoundIcon />,
+	textButton: <ButtonSquareText />
 }
 
 export default class AtomsButton extends React.Component{
