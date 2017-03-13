fe306658d03969a77216410fcc61a7ad7e21a822
Banks, Martin <anotherbanksy@gmail.com>
2017-03-13 17:30:54 +1100

    add expander button

---
+++ src/modules/atoms/buttons/ShowMore.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import style from './buttonStyles.css'
+import MaterialIcon from '../icon/MaterialIcon.js'
+
+
+let showMoreState = {
+	isActive: false,
+	text: 'Show more',
+	icon: 'expand_more'
+}
+let showLessState = {
+	isActive: true,
+	text: 'Show less',
+	icon: 'expand_less'
+}
+
+
+export default class ButtonShowMore extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = showMoreState
+		this.changeState = this.changeState.bind(this)
+	}
+
+	changeState(){
+		this.setState(this.state.isActive ? showMoreState : showLessState)
+		//this.props.handleClick(this.state.isActive)
+		this.props.handleClick()
+	}
+
+	render(){
+		return (
+			<button
+				className={style.showMore}
+				onClick={this.changeState}
+				data-active = {this.state.isActive}
+			>
+				<span>{this.state.text}</span>
+				<MaterialIcon icon={this.state.icon}/>
+			</button>
+		)
+	}
+}
+
+
+ButtonShowMore.propTypes = {
+	text: React.PropTypes.string
+}
+
+
