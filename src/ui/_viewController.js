55420bd523dcb542ace4e53710f4dc15afecd5a1
Banks, Martin <anotherbanksy@gmail.com>
2017-03-14 19:08:39 +1100

    add image atoms, timeline card molecule and twin timeline components

--- src/ui/_viewController.js
+++ src/ui/_viewController.js
@@ -3,11 +3,14 @@ import AtomsText from './views/AtomsText'
 import AtomsColor from './views/AtomsColor'
 import AtomsButton from './views/AtomsButton'
 import AtomsContainer from './views/AtomsContainer'
+import AtomsImage from './views/AtomsImage'
 
 import MoleculeHeaderText from './views/MoleculeHeader'
 import MoleculeBodyText from './views/MoleculeBody'
+import Molecule_TimelineCard from './views/Molecule_TimelineCard'
 
 import Component_CardSimple from './views/Component_CardSimple'
+import Component_Timeline_Split from './views/Component_Timeline_Split'
 
 export const viewController = {
 	atoms: {
@@ -22,22 +25,34 @@ export const viewController = {
 		},
 		container: {
 			template: <AtomsContainer />
+		},
+		images: {
+			template: <AtomsImage />
 		}
 	},
+
 	molecules: {
 		"header text": {
 			template: <MoleculeHeaderText />
 		},
 		"body text": {
 			template: <MoleculeBodyText />
+		},
+		"timeline card": {
+			template: <Molecule_TimelineCard />
 		}
 	},
+
 	components: {
 		"simple card": {
 			template: <Component_CardSimple />
+		},
+		"twin timeline": {
+			template: <Component_Timeline_Split />
 		}
 	},
-	templates: {
+	
+	/*templates: {
 		"first view": {}
-}
+	}*/
 }
