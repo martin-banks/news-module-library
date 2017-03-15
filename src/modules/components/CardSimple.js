bd221dbc89d3f41b919a9faf46b435f5a7713b02
Banks, Martin <anotherbanksy@gmail.com>
2017-03-15 19:28:09 +1100

    refactor content handling. test animations (and comment out).

--- src/modules/components/CardSimple.js
+++ src/modules/components/CardSimple.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
 import {sampleText} from '../../sampleContent/text'
 
-import styles from '../molecules/HeaderText.css'
+import styles from './_CardSimple.css'
 
 import Card from '../atoms/containers/Card'
 import ShowMore from '../atoms/buttons/ShowMore'
 import BodyContent from '../molecules/BodyText'
 import HeaderText from '../molecules/HeaderText'
+
 
 
 
@@ -37,6 +40,10 @@ const bodyContent = [
 ]
 const headerContent = [
 	{
+		type: 'Image_translucentShadow',
+		imagePath: 'https://unsplash.it/200/300/?random'
+	},
+	{
 		type: 'Kicker',
 		content: 'Lorem ipsum etc ...'
 	},
@@ -50,24 +57,32 @@ const headerContent = [
 	}
 ]
 
-
+/*
 const openContent = p => [
-	<HeaderText content={p.header} />,
-	<BodyContent content={p.body} />
-]
-const closedContent = p =>  [
-	<HeaderText content={p.header} />
+		<HeaderText key="header__key__1" content={p.header} />
+	,
+	<BodyContent style={styles.bodyContentContainer} key="body__key__1" content={p.body} /> 
 ]
 
+const closedContent = p =>  [
+	<HeaderText  key="body__key__1" content={p.header} />
+]*/
+
+
+
+
+const makeBodyContent = p => <BodyContent key="body__key__1" content={p.body} />
 
 export default class CardSimple extends React.Component {
 	constructor(props) {
 		super(props)
 		this.toggleOpen = this.toggleOpen.bind(this)
-
 		this.state = {
 			isOpen: false,
-			content: null,
+			content: {
+				head: null,
+				body: null
+			},
 			cardContent: {
 				body: bodyContent,
 				header: headerContent
@@ -78,28 +93,41 @@ export default class CardSimple extends React.Component {
 	toggleOpen(update){
 		if(this.state.isOpen){
 			/* the card is open - time to close it */
-			this.setState({ isOpen: false })
-			this.setState({ content: closedContent(this.state.cardContent) })
+			let newState = this.state
+			newState.isOpen = false
+			//newState.content.body = <div>'text gone'</div>
+			this.setState( newState )
+
+			//this.setState({ content: closedContent(this.state.cardContent) })
 		} else {
 			/* the card is closed - time to open it */
-			this.setState({ isOpen: true })
-			this.setState({ content: openContent(this.state.cardContent) })
+			let newState = this.state
+			newState.isOpen = true
+			//newState.content.body = makeBodyContent(this.state.cardContent)
+			this.setState( newState )
+			
+			//this.setState({ isOpen: true })
+			//this.setState({ content: openContent(this.state.cardContent) })
 		}
 	}
 
 	componentWillMount() {
-		this.setState({ content: closedContent(this.state.cardContent) })
+		let newState = this.state
+		newState.isOpen = false
+		newState.content.head = <HeaderText  key="header__key__1" content={this.state.cardContent.header} />
+		newState.content.body = <BodyContent key="body__key__1" content={this.state.cardContent.body} />
+		this.setState( newState )
+	
+		//this.setState({ content: closedContent(this.state.cardContent) })
 	}
 
 	render(){
 		return (
-			<div className={styles.headerTextContainer} >
-				<Card>
-					{this.state.content}
-					<ShowMore handleClick = {this.toggleOpen} />
-				</Card>
-				
-			</div>
+			<Card>
+				{this.state.content.head}
+				{this.state.isOpen ? this.state.content.body : null }
+				<ShowMore handleClick = {this.toggleOpen} />
+			</Card>
 		)
 	}
 
@@ -108,3 +136,32 @@ export default class CardSimple extends React.Component {
 HeaderText.propTypes = {
 
 }*/
+
+
+/*
+
+<ReactCSSTransitionGroup
+	//transitionName="example"
+	//transitionName={ styles }
+	transitionName = {{
+		enter: styles.enter,
+		enterActive: styles.enterActive,
+		leave: styles.leave,
+		leaveActive: styles.leaveActive,
+		appear: styles.appear,
+		appearActive: styles.appearActive
+	}}
+	transitionAppear={true}
+	transitionAppearTimeout={2000}
+	transitionEnter={true}
+	transitionEnterTimeout={2000}
+	transitionLeave={true}
+	transitionLeaveTimeout={2000}
+>
+	<div className={styles.bodyContentContainer} >
+		{this.state.content}
+	</div>
+	
+</ReactCSSTransitionGroup>
+
+*/
