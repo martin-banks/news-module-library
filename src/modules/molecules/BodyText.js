a71fe761922879db0eab970bd3bbff7be8636e6d
Banks, Martin <anotherbanksy@gmail.com>
2017-03-15 19:25:57 +1100

    updates for animations"

--- src/modules/molecules/BodyText.js
+++ src/modules/molecules/BodyText.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
 import {sampleText} from '../../sampleContent/text'
 
 import styles from './HeaderText.css'
@@ -13,10 +15,10 @@ import ListText from '../../modules/atoms/text/ListText'
 
 
 const supportedBlocks = {
-	BodyText: p => <BodyText text={p.content} style={p.style} />,
-	Crosshead: p =>  <Crosshead text={p.content}/>,	
-	SmallHead: p => <Smallhead text={p.content}/>,
-	ListText: p => <ListText type={p.style} items={p.content} />
+	BodyText: (p,i) => <BodyText key={`key__${i}`} text={p.content} style={p.style} />,
+	Crosshead: (p,i) =>  <Crosshead key={`key__${i}`} text={p.content}/>,	
+	SmallHead: (p,i) => <Smallhead key={`key__${i}`} text={p.content}/>,
+	ListText: (p,i) => <ListText key={`key__${i}`} type={p.style} items={p.content} />
 }
 
 
@@ -24,21 +26,61 @@ export default class BodyContent extends React.Component {
 	constructor(props) {
 		super(props)
 		this.processContent = this.processContent.bind(this)
+		//this.addContent = this.addContent.bind(this)
+		this.state = { 
+			content: [
+				
+			]
+		}
 	}
-
 
 
 	processContent(){
-		return this.props.content.map( block => {
-			return supportedBlocks[block.type](block)
-		} )
+		if(!!this.props.content){
+			return this.props.content.map( (block,i) => {
+				return supportedBlocks[block.type](block,i)
+			})
+		} else { 
+			return null
+		}
+
+		
 	}
+
+	componentWillMount() {
+		this.setState({content: this.processContent()})
+	}
+
+	/*componentWillUpdate() {
+		this.setState({content: this.processContent() })
+	}*/
 
 	render(){
 		return (
-			<div className={styles.headerTextContainer} >
-				{this.processContent()}
-			</div>
+			<ReactCSSTransitionGroup
+				component='div'
+				//transitionName="example"
+				//transitionName={ styles }
+				transitionName = {{
+					enter: styles.enter,
+					enterActive: styles.enterActive,
+					leave: styles.leave,
+					leaveActive: styles.leaveActive,
+					appear: styles.appear,
+					appearActive: styles.appearActive
+				}}
+				transitionAppear={true}
+				transitionAppearTimeout={500}
+				transitionEnter={true}
+				transitionEnterTimeout={500}
+				transitionLeave={true}
+				transitionLeaveTimeout={1000}
+			>
+				<div className={styles.bodyContentContainer} >
+					{this.state.content}
+				</div>
+				
+			</ReactCSSTransitionGroup>
 		)
 	}
 
@@ -46,7 +88,8 @@ export default class BodyContent extends React.Component {
 
 
 BodyContent.propTypes = {
-	content: React.PropTypes.array.isRequired
+	content: React.PropTypes.array.isRequired,
+	//show: React.PropTypes.boolean
 }
 
 
--- src/modules/molecules/HeaderText.js
+++ src/modules/molecules/HeaderText.js
@@ -2,31 +2,35 @@ import React from 'react'
 
 import Kicker from '../atoms/text/Kicker'
 import Title from '../atoms/text/Title'
+//import Crosshead from '../atoms/text/Crosshead'
 import Intro from '../atoms/text/Intro'
 import styles from './HeaderText.css'
+import Image_translucentShadow from '../atoms/images/Image_translucentShadow'
 
-
+/*
 const headerContent = [
 	{
 		type: 'Kicker',
 		content: 'Lorem ipsum etc ...'
 	},
 	{
-		type: 'Title',
+		type: 'Crosshead',
 		content: 'Lorem ipsum etc ...'
 	},
 	{
 		type: 'Intro',
 		content: 'Lorem ipsum etc ...'
 	}
-]
+]*/
 
 
 
 const supportedBlocks = {
-	Kicker: p => <Kicker text={p.content} style={p.style} />,
-	Title: p => <Title text={p.content}/>,	
-	Intro: p => <Intro text={p.content}/>
+	Kicker: p => <Kicker key="kicker__key__1" text={p.content} style={p.style} />,
+	Title: p => <Title key="title__key__1" text={p.content}/>,
+	//Crosshead: p => <Crosshead key="__key__1" text={p.content} />,
+	Intro: p => <Intro key="intro__key__1" text={p.content}/>,
+	'Image_translucentShadow': p => <Image_translucentShadow key="image__key__1" imagePath={p.imagePath} addStyles={p.addStyles}/>
 }
 
 
@@ -51,7 +55,7 @@ export default class HeaderText extends React.Component {
 	}
 
 }
-/*
-HeaderText.propTypes = {
 
-}*/
+HeaderText.propTypes = {
+	content: React.PropTypes.array.isRequired
+}
